fcc4767feb382fb5dbc7478529ed43b718bd524c
viniciussis <vinicius01012@gmail.com>
2025-10-06 10:37:09 -0300

    feat: add and run prettier

--- src/leetcodes/two-pointers/longest-palindrome/index.ts
+++ src/leetcodes/two-pointers/longest-palindrome/index.ts
@@ -1,29 +1,31 @@
-import { isPalindrome } from "../../../utils";
+import { isPalindrome } from '../../../utils';
 
 export function bruteForcelongestPalindrome(s: string): string {
-    if (s.length === 1) return s;
+  if (s.length === 1) return s;
 
-    let left = 0, right = s.length - 1;
-    let inner_palindrome: string = "";
-    let outer_palindrome: string = "";
-    let resp = "";
-    
-    while (left < right) {
-        outer_palindrome = s.slice(left, right + 1)
-        if (isPalindrome(outer_palindrome) && outer_palindrome.length == s.length) {
-            resp = outer_palindrome;
-            break;
-        };
+  let left = 0,
+    right = s.length - 1;
+  let inner_palindrome: string = '';
+  let outer_palindrome: string = '';
+  let resp = '';
 
-        for (let i = 0; i < outer_palindrome.length; i++) {
-            inner_palindrome = outer_palindrome;
-            if (i != 0) inner_palindrome = s.slice(i, outer_palindrome.length + 1);
-            if (isPalindrome(inner_palindrome) && inner_palindrome.length > resp.length) {
-                resp = inner_palindrome;
-            };
-        }
-
-        right--;
+  while (left < right) {
+    outer_palindrome = s.slice(left, right + 1);
+    if (isPalindrome(outer_palindrome) && outer_palindrome.length == s.length) {
+      resp = outer_palindrome;
+      break;
     }
-    return resp;
-};
+
+    for (let i = 0; i < outer_palindrome.length; i++) {
+      inner_palindrome = outer_palindrome;
+      if (i != 0) inner_palindrome = s.slice(i, outer_palindrome.length + 1);
+      if (isPalindrome(inner_palindrome) && inner_palindrome.length > resp.length) {
+        resp = inner_palindrome;
+      }
+    }
+
+    right--;
+  }
+
+  return resp;
+}
--- src/leetcodes/two-pointers/remove-duplicates/index.ts
+++ src/leetcodes/two-pointers/remove-duplicates/index.ts
@@ -1,25 +1,27 @@
-function removeDuplicates(nums: number[]) {
-    let left = 0, right = nums.length - 1;
-    let counter = 0;
-    while (left < right) {
-        let temp = nums[right]
-        if (temp === nums[right - 1]) {
-            if (temp) {
-                nums.unshift(temp)
-                nums.pop()
-                nums.pop()
-                right--;
-                continue;
-            }
-        }
-        if (temp) {
-            nums.unshift(temp)
-            nums.pop()
-        }
-        left++;
+export function removeDuplicates(nums: number[]) {
+  let left = 0,
+    right = nums.length - 1;
+  let counter = 0;
+  while (left < right) {
+    let temp = nums[right];
+    if (temp === nums[right - 1]) {
+      if (temp) {
+        nums.unshift(temp);
+        nums.pop();
+        nums.pop();
+        right--;
+        continue;
+      }
     }
-    return { nums, length: nums.length };
-};
+    if (temp) {
+      nums.unshift(temp);
+      nums.pop();
+    }
+    left++;
+  }
 
-console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
+  return { nums, length: nums.length };
+}
+
+// console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
 // console.log(removeDuplicates([1,1,2]))
