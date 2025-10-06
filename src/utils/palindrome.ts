0c0019c42e7cf8b78b5448c08f8cff38f5e8c5b2
Vinícius Silva Santos <vinicius01012@gmail.com>
2025-10-06 15:28:05 -0300

    refact: add default values to palidrome range function

--- src/utils/palindrome.ts
+++ src/utils/palindrome.ts
@@ -1,9 +1,9 @@
 export function isPalindrome(s: string): boolean {
-    const str = s.toLowerCase().replace(/[^a-z0-9]/g, '');
-    return str === str.split('').reverse().join('');
-};
+  const str = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+  return str === str.split('').reverse().join('');
+}
 
-export function isPalindromeRange(s: string, left: number, right: number): boolean {
+export function isPalindromeRange(s: string, left: number = 0, right: number = s.length - 1): boolean {
   while (left < right) {
     if (s[left] !== s[right]) return false;
     left++;
